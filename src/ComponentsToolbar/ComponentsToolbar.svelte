7bc9755f14a854bc2cfa8fce827b294d93d2e5c3
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-11-19 14:06:21 +0200

    Components

--- src/ComponentsToolbar/ComponentsToolbar.svelte
+++ src/ComponentsToolbar/ComponentsToolbar.svelte
@@ -1,31 +1,32 @@
 <script>
-    import { Button } from "flowbite-svelte";
-    import { GearSolid } from "flowbite-svelte-icons";
+  import { Button } from "flowbite-svelte";
+  import { GearSolid } from "flowbite-svelte-icons";
 
-    export let onConfig = ()=>{}
-  </script>
-  
-  <div class="container">
-    <div class="toolbar">
-      <Button on:click={onConfig} class="Button" size="xs" color="alternative"><GearSolid size="xs" /></Button>
-    </div>
+  export let onConfig = () => {};
+</script>
+
+<div class="container">
+  <div class="toolbar">
+    <Button on:click={onConfig} class="Button" size="xs" color="alternative"
+      ><GearSolid size="xs" /></Button
+    >
   </div>
-  
-  <style>
-    .container:hover .toolbar {
-      opacity: 1;
-      visibility: visible;
-    }
-  
-    .toolbar {
-      opacity: 0;
-      visibility: hidden;
-      pointer-events: auto;
-      transition: opacity 0.3s, visibility 0.3s; /* Add a transition for smooth fade-in/out */
-    }
-  
-    .toolbar .Button {
-      margin-right: 10px;
-    }
-  </style>
-  
+</div>
+
+<style>
+  .container:hover .toolbar {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  .toolbar {
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: auto;
+    transition: opacity 0.3s, visibility 0.3s; /* Add a transition for smooth fade-in/out */
+  }
+
+  .toolbar .Button {
+    margin-right: 10px;
+  }
+</style>
