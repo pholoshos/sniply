03b1c6570abe2e6fd9e344699d67308a10c3d21e
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-11-19 11:28:43 +0200

    feat : clean up

--- src/routes/build/+page.svelte
+++ src/routes/build/+page.svelte
@@ -1,14 +1,5 @@
 <script>
-  import AppContainer from "$lib/AppContainer/AppContainer.svelte";
-import DraggableComponent from "$lib/DraggableComponent/DraggableComponent.svelte";
   import WorkSpace from "$lib/workspace/workSpace.svelte";
-  import { Badge } from "flowbite-svelte";
-    
 </script>
 
-<!-- <AppContainer></AppContainer> -->
-<WorkSpace/>
-<!-- <DraggableComponent></DraggableComponent> -->
-<!-- <DraggableComponent></DraggableComponent>
-<DraggableComponent></DraggableComponent>
-<DraggableComponent></DraggableComponent> -->
+<WorkSpace />
