0aa6386bb9486c35e705170f6b4db0159b0d1fd1
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-11-25 13:46:47 +0200

    Remove unused imports and components

--- src/lib/AppContainer/AppContainer.svelte
+++ src/lib/AppContainer/AppContainer.svelte
@@ -1,9 +1,6 @@
 <script>
-  import ConfigurationTab from "$lib/ConfigurationTab/ConfigurationTab.svelte";
   import DraggableComponent from "$lib/DraggableComponent/DraggableComponent.svelte";
   import AppNavbar from "$lib/navbar/AppNavbar.svelte";
-  import { Button } from "flowbite-svelte";
-  import { DownloadSolid } from "flowbite-svelte-icons";
 
   export let backgroundColor = "white";
   export let appName = "sniply";
