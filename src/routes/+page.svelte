729b7ea35dd1c1d3c3f6c61673450cb26fe21165
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-11-26 02:13:18 +0200

    Add authorization headers and redirect to home page on mount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,20 @@
   import GentChart from "$lib/GentChart/GentChart.svelte";
   import MainLayout from "$lib/mainLayout/MainLayout.svelte";
   import { Button } from "flowbite-svelte";
-</script>
+  import { api, generalApi } from "../utils";
+  import { goto } from "$app/navigation";
+  import user from "../store/user";
+  import { onMount, afterUpdate } from "svelte";
+  import { getData, getToken } from "../utils/localStorage";
 
-<MainLayout pageName="Dashboard"></MainLayout>
+  onMount(() => {
+    try{
+      let token = getToken();
+      api.setHeader("Authorization", `Bearer ${token}`);
+      generalApi.setHeader("Authorization", `Bearer ${token}`);
+      goto("/pages/home", { replaceState: true });
+    }catch(e){
+      console.log(e);
+    }
+  });
+</script>
