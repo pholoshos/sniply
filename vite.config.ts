e1b7fc4e5012931869eb3e407f1adb54d87e8227
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-12-17 10:54:53 +0200

    Fix baseApiUrl assignment in utils/index.ts and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	build: {
+		target: "esnext" // or "es2019",
+	},
 	server: {
 		// Add the allowed paths
 		fs: {
